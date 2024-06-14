2bc1d1197b2a178b8b30b82198c7224116797330
ahmadsofuwan <sofuwan62@gmail.com>
2024-06-15 01:58:52 +0700

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,71 +1,136 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/OneMonBot.png') }}">
-    <title>Xwdoge</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap:">
+
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
+
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700;800;900&family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>DogeStar</title>
+    <link rel="stylesheet" href="{{ asset('buzzle') }}/css/framework7.bundle.css">
+    <link rel="stylesheet" href="{{ asset('buzzle') }}/css/style.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
 
-<body class="text-gray-800 antialiased bg-gray-100">
-    <header class="flex justify-between items-center p-4 bg-white shadow-md">
-        <div class="text-lg font-bold flex">
-            <img src="{{ asset('img/logo.png') }}" alt="" class="w-10">
-            <a href="{{ route('login') }}" class="uppercase my-auto"> Xwdoge</a>
-        </div>
-    </header>
-    <main class="flex items-center justify-center min-h-screen">
-        <div class="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-            <div class="text-left">
-                <h2 class="text-3xl font-semibold text-gray-900 inline-block px-2 py-1 rounded">Sign In.</h2>
+<body>
+    <div class="view view-main view-init ios-edges" data-url="/">
+        <div class="page page-home page-with-subnavbar">
+
+
+            <div class="tabs">
+                <div id="tab-home" class="tab tab-home tab-active">
+                    <!-- ===== TAB HOME ===== -->
+                    <div class="pb-5 ">
+                        <div class="page bg-black">
+                            <div class="navbar navbar-transparent">
+                                <div class="navbar-bg"></div>
+                                <div class="navbar-inner sliding">
+                                    <div class="left">
+                                        <a class="link back">
+                                            <i class="icon icon-back"></i>
+                                        </a>
+                                    </div>
+                                    <div class="title">Sign In</div>
+                                </div>
+                            </div>
+                            <div class="page-content">
+                        
+                                <!-- separator -->
+                                <div class="separator-large"></div>
+                                <!-- end separator -->
+                        
+                                <!-- sign in -->
+                                <div class="sign-in">
+                                    <div class="content text-center">
+                                        <img class="image-illustration" src="{{ asset('img/logo.png') }}" alt="">
+                                        <h3>Welcome To DogeStar!</h3>
+                                    </div>
+                        
+                                    <!-- separator -->
+                                    <div class="separator-large"></div>
+                                    <!-- end separator -->
+                        
+                                    <div class="content-form">
+                                        <form class="list" method="post" action="{{ route('login.auth') }}">
+                                            @csrf
+                                            <ul>
+                                                <li class="item-content item-input">
+                                                    <div class="item-inner">
+                                                        <div class="item-input-wrap">
+                                                            <input type="text" placeholder="Username" name="username">
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li class="item-content item-input">
+                                                    <div class="item-inner">
+                                                        <div class="item-input-wrap">
+                                                            <input type="password" placeholder="Password" name="password">
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <div class="button-default ">
+                                                <div class="container">
+                                                    <button class="button bg-yellow-500 text-black">Sign In</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                        
+                                        <!-- separator -->
+                                        <div class="separator-medium"></div>
+                                        <!-- end separator -->
+                        
+                                        <div class="link-forgot text-center text-small">
+                                            <a href="#" class="">Forgot Password?</a>
+                                        </div>
+                        
+                                    </div>
+                                </div>
+                                <!-- end sign in -->
+                        
+                                <!-- separator -->
+                                <div class="separator-large"></div>
+                                <!-- end separator -->
+                        
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                    <!-- ===== END TAB HOME ===== -->
+                </div>
+
             </div>
-            <form class="mt-8 space-y-6" method="post" action="{{ route('login.auth') }}">
-                @csrf
-                <div class="rounded-md shadow-sm space-y-4">
 
-                    <div class="relative flex items-center">
-                        <span class="pl-3 flex items-center pointer-events-none mr-2">
-                            <i class="fas fa-wallet text-gray-400"></i>
-                        </span>
-                        <input id="username" name="username" type="text" autocomplete="username" required class="appearance-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm rounded-full" placeholder="Dogochain Wallet Address" value="{{ old('username') }}">
-                    </div>
-
-                    <div class="relative flex items-center">
-                        <span class="pl-3 flex items-center pointer-events-none mr-2">
-                            <i class="fas fa-lock text-gray-400"></i>
-                        </span>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm rounded-full" placeholder="Password" value="{{ old('password') }}">
-                    </div>
-
-                </div>
-                <div class="flex items-center justify-between">
-                    <div class="text-sm">
-                        <a href="{{ route('forget') }}" class="font-medium text-gray-600 hover:text-gray-900">Forgot Password?</a>
-                    </div>
-                </div>
-                <div>
-                    <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                        ACCESS MY ACCOUNT
-                        <span class="absolute inset-y-0 right-0 flex items-center pl-2">
-                            <i class="fas fa-arrow-right text-gray-400"></i>
-                        </span>
-                    </button>
-                </div>
-                <div class="text-center">
-                    <p class="mt-2 text-sm text-gray-600">Don't have an account? <a href="{{ route('register') }}" class="font-medium text-gray-600 hover:text-gray-900">Register</a></p>
-                    <p class="mt-2 text-sm text-gray-600">Need to create a wallet? <a href="https://chromewebstore.google.com/detail/unielon/inlkhilmjmjomfcpdifpfgllhhlpnbej" class="font-medium text-gray-600 hover:text-gray-900" target="_blank">Create Wallet</a></p>
-                </div>
-            </form>
         </div>
-    </main>
+    </div>
+
+
+    <!-- script -->
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <script src="{{ asset('js/jquery-3.6.3.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- end script -->
+    <script>
+        function cpy() {
+            $('.btn-cpy').click(async function() {
+                var text = $(this).data('link');
+                const shareData = {
+                    text: text
+                };
+                await navigator.share(shareData);
+            });
+        }
+        cpy()
+    </script>
     @include('sweetalert::alert')
 </body>
 
